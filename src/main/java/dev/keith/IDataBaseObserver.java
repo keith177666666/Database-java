2d65034c82d14437fd8f07e76293a2df6805629c
keith17766666 <109770334+keith17766666@users.noreply.github.com>
2024-12-31 10:14:29 +0800

    fixing bugs

--- src/main/java/dev/keith/IDataBaseObserver.java
+++ src/main/java/dev/keith/IDataBaseObserver.java
@@ -1,7 +1,7 @@
 package dev.keith;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.util.List;
 import java.util.function.Function;
 
@@ -9,9 +9,9 @@ public interface IDataBaseObserver<V, D extends IData<V>, S extends IDataBaseObs
     S getSerializer();
     Function<V, D> getFactory();
     interface Serializer<D extends IData<?>> {
-        D deserialize(InputStream dataInput);
-        ResultType serialize(D data, OutputStream dataOutput);
-        default ResultType serialize(List<D> data, OutputStream dataOutput) {
+        D deserialize(BufferedReader dataInput);
+        ResultType serialize(D data, BufferedWriter dataOutput);
+        default ResultType serialize(List<D> data, BufferedWriter dataOutput) {
             for(D data1 : data) {
                 if(!(serialize(data1, dataOutput).canContinue)) {
                     return ResultType.EXCEPTION;
